243c79ea8455675cfafe67aac1e99524f4678b7d
Brian Tai <btai@soxhub.com>
2020-04-18 16:24:05 -0700

    use a variables file

--- runner-entrypoint.sh
+++ runner-entrypoint.sh
@@ -4,13 +4,14 @@
 if [ -f $GITHUB_EVENT_PATH ]; then
   # TODO: remove, for debugging only
 	cat $GITHUB_EVENT_PATH
-	
+
+	touch /tmp/variables.yaml
+
 	# Codefresh system provided variables
 	# https://codefresh.io/docs/docs/codefresh-yaml/variables/#system-provided-variables
-	touch /tmp/codefresh_system_variables
-	echo "CF_REVISION=$(cat $GITHUB_EVENT_PATH | jq -r .head_commit.id)" > /tmp/codefresh_system_variables
-	echo "REPO_ORG=$(cat $GITHUB_EVENT_PATH | jq -r .repository.organization)" > /tmp/codefresh_system_variables
-	echo "REPO_NAME=$(cat $GITHUB_EVENT_PATH | jq -r .repository.name)" > /tmp/codefresh_system_variables
+	echo -e "- CF_REVISION=$(cat $GITHUB_EVENT_PATH | jq -r .head_commit.id)\n" > /tmp/variables.yaml
+	echo -e "  CF_REPO_OWNER=$(cat $GITHUB_EVENT_PATH | jq -r .repository.organization)\n" > /tmp/variables.yaml
+	echo -e "  CF_REPO_NAME=$(cat $GITHUB_EVENT_PATH | jq -r .repository.name)\n" > /tmp/variables.yaml
 
 	# in case of push event
 	BRANCH=$(cat $GITHUB_EVENT_PATH | jq -r .ref | awk -F '/' '{print $3}')
@@ -29,7 +30,7 @@ codefresh auth use-context context
 
 if [ -n "$TRIGGER_NAME" ]
 then
-	codefresh run $PIPELINE_ID --trigger=$TRIGGER_NAME --branch=$BRANCH --var-file=/tmp/codefresh_system_variables
+	codefresh run $PIPELINE_ID --trigger=$TRIGGER_NAME --branch=$BRANCH --var-file=/tmp/variables.yaml
 else
-	codefresh run $PIPELINE_ID --branch=$BRANCH --var-file=/tmp/codefresh_system_variables
+	codefresh run $PIPELINE_ID --branch=$BRANCH --var-file=/tmp/variables.yaml
 fi
