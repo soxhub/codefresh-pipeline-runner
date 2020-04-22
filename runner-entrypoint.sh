e4c634fc6c35f7f83fd903abd2ccdf63932ffc25
Brian Tai <btai@soxhub.com>
2020-04-21 23:11:29 -0700

    comment out for loop

--- runner-entrypoint.sh
+++ runner-entrypoint.sh
@@ -5,8 +5,6 @@ if [ -f $GITHUB_EVENT_PATH ]; then
   # TODO: remove, for debugging only
   cat $GITHUB_EVENT_PATH
 
-  echo $CF_API_KEY
-
   touch /tmp/variables.json
 
   # Codefresh system provided variables
@@ -30,9 +28,9 @@ if [ -f $GITHUB_EVENT_PATH ]; then
 
   # Env vars set with prefix 'CFVAR_' will be set as variables passed into codefresh with the 'CFVAR_' prefix removed
   # i.e. CFVAR_HELM_REPO_NAME=my-helm-repo will be passed to Codefresh as HELM_REPO_NAME=my-helm-repo
-  for var in "${!CFVAR_@}"; do
-    echo $(cat /tmp/variables.json | jq --arg key ${var#"CFVAR_"} --arg value ${!var} '[.[0] + {"\($key)": "\($value)"}]') > /tmp/variables.json
-  done
+  # for var in "${!CFVAR_@}"; do
+  #   echo $(cat /tmp/variables.json | jq --arg key ${var#"CFVAR_"} --arg value ${!var} '[.[0] + {"\($key)": "\($value)"}]') > /tmp/variables.json
+  # done
 
   cat /tmp/variables.json
 
